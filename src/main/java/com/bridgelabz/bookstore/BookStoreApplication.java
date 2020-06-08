8e88e8e3a75e51029ef2d22e291a34b50959ce17
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-06-08 09:29:16 -0400

    Added:Code to use Corn job Schedular

--- src/main/java/com/bridgelabz/bookstore/BookStoreApplication.java
+++ src/main/java/com/bridgelabz/bookstore/BookStoreApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@Configuration
 @SpringBootApplication
 @EnableCaching
-//@EnableSwagger2
+@EnableScheduling
 public class BookStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
---
+++ src/main/java/com/bridgelabz/bookstore/schedular/TaskSchedular.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.bookstore.schedular;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskSchedular {
+    //@Scheduled(fixedDelay = 10000)
+    @Scheduled(cron = "* * * 25 12 ? ")
+    public void taskScheduling(){
+        System.out.println("Welcome to Task scheduleing"+new java.util.Date());
+    }
+}
